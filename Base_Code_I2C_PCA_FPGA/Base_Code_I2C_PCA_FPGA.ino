35a6980985e17ba33a750920674b82277e013407
roca-systems <ffrodrc@gmail.com>
2024-06-24 20:15:17 -0600

    Codigos Finales

--- Base_Code_I2C_PCA_FPGA/Base_Code_I2C_PCA_FPGA.ino
+++ Base_Code_I2C_PCA_FPGA/Base_Code_I2C_PCA_FPGA.ino
@@ -7,29 +7,33 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(PCA9685_ADDRESS);
 
+const int ledPin = 10;
 
-        int matrizA[8][4] = {
-        {300, 300, 300, 300},
-        {394, 351, 347, 300},
-        {402, 424, 227, 300},
-        {402, 424, 227, 393},
-        {402, 357, 227, 393},
-        {314, 357, 221, 393},
-        {314, 418, 221, 393},
-        {300, 300, 300, 300}
+int matrizB[12][4] = {
+        {300, 300, 300, 390},
+        {422, 342, 366, 390},
+        {422, 397, 288, 390},
+        {423, 410, 274, 390},
+        {423, 410, 274, 498},
+        {423, 410, 225, 498},
+        {423, 339, 305, 498},
+        {332, 339, 305, 498},
+        {332, 339, 305, 498},
+        {332, 339, 305, 390},
+        {340, 338, 245, 390},
+        {300, 300, 300, 390}
     };
 
-    int matrizB[10][4] = {
-        {300, 300, 300, 300},
-        {361, 368, 300, 300},
-        {397, 400, 321, 300},
-        {397, 455, 285, 300},
-        {397, 455, 285, 375},
-        {397, 353, 292, 375},
-        {321, 345, 292, 375},
-        {281, 360, 403, 375},
-        {281, 360, 403, 300},
-        {300, 300, 300, 300}
+        int matrizA[9][4] = {
+        {300, 300, 300, 390},
+        {398, 327, 373, 390},
+        {394, 441, 300, 390},
+        {394, 441, 300, 498},
+        {399, 360, 300, 498},
+        {281, 360, 300, 498},
+        {281, 345, 400, 498},
+        {281, 345, 400, 390},
+        {300, 300, 300, 390}
     };
 
 
@@ -38,6 +42,7 @@ byte out;
 int servo1 = 0; // Número del canal para el primer servo
 
 void setup() {
+    pinMode(ledPin, OUTPUT);
   // Inicializa el bus I2C
   Wire.begin();
 
@@ -76,31 +81,44 @@ void loop() {
     Serial.println(out, DEC);
 
     // Controla el servo motor basado en el valor recibido
-      if (out == 1) {
-     for(int i=0;i < 10; i++){
-  pwm.setPWM(0, 0, matrizB[i][0]);
-  pwm.setPWM(1, 0, matrizB[i][1]);
-  pwm.setPWM(2, 0, matrizB[i][2]);
-  pwm.setPWM(3, 0, matrizB[i][3]);
-  delay(1000);
-    }
-      }
-
-            if (out == 2) {
-     for(int i=0;i < 8; i++){
+//posiscion de servos
+if (out == 1){
+  
+for(int i=0;i < 9; i++){
   pwm.setPWM(0, 0, matrizA[i][0]);
   pwm.setPWM(1, 0, matrizA[i][1]);
   pwm.setPWM(2, 0, matrizA[i][2]);
   pwm.setPWM(3, 0, matrizA[i][3]);
   delay(1000);
-    }
+}
+}
+
+ delay(5000);
+
+ if (out == 2){
+for(int i=0;i <12; i++){
+  if(i==9){
+    analogWrite(ledPin, 100);
+    delay(300);
+    analogWrite(ledPin, 0);
+    delay(100);
+    analogWrite(ledPin, 100);
+    delay(300);
+    analogWrite(ledPin, 0);
+    delay(100);
       }
+  pwm.setPWM(0, 0, matrizB[i][0]);
+  pwm.setPWM(1, 0, matrizB[i][1]);
+  pwm.setPWM(2, 0, matrizB[i][2]);
+  pwm.setPWM(3, 0, matrizB[i][3]);
+  delay(1000);
+}
+ }
 
   } else {
     // No hay datos en el puerto serial
     Serial.println("Esperando dato del puerto serial...");
     delay(5000); // Esperar un poco antes de comprobar nuevamente
   }
-        // Mueve el primer servo en el canal 0
 
 }
